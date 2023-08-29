af357a73fe8399d5c5c6aa0e515ccfb44793d8f7
Kamil Kucharczyk <kamil.kucharczyk007@gmail.com>
2023-08-29 14:09:47 +0200

    js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 100)
+);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(storedTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const emailBox = document.querySelector('input');
+const messageBox = document.querySelector('textarea');
+const form = document.querySelector('.feedback-form');
+
+const currentInput = {
+  email: emailBox.value,
+  message: messageBox.value,
+};
+
+followStorage = () => {
+  const storedInput = localStorage.getItem('feedback-form-state');
+  if (storedInput) {
+    const { email, message } = JSON.parse(storedInput);
+    (emailBox.value = email), (messageBox.value = message);
+  }
+};
+window.addEventListener('load', followStorage);
+
+const loadInput = () => {
+  currentInput.email = emailBox.value;
+  currentInput.message = messageBox.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentInput));
+};
+
+form.addEventListener('input', throttle(loadInput, 500));
+
+const loadSubmit = e => {
+  e.preventDefault();
+  if (emailBox.value === '' || messageBox.value === '') {
+    console.log('Please fill in all the fields!');
+  } else {
+    console.log(currentInput);
+    localStorage.removeItem('feedback-form-state');
+    form.reset();
+  }
+};
+
+form.addEventListener('submit', loadSubmit);
